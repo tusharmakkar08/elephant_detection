394eeef3f70ec50fb6fb034b31bfb1a6710d5557
Tushar Makkar <tusharmakkar08@gmail.com>
2015-04-13 11:31:27 +0530

    Image resizing function written

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import cv2, numpy
+import cv2, numpy, os
 from sklearn import svm
 
 def trainSample(path, totalNo):
@@ -10,9 +10,9 @@ def trainSample(path, totalNo):
     out_train = []
     for i in xrange(1,totalNo):
         if i<10:
-            image_no = path + "image_000" + str(i) + ".jpg"
+            image_no = path + "imagemod_000" + str(i) + ".jpg"
         else:
-            image_no = path + "image_00" + str(i) + ".jpg"
+            image_no = path + "imagemod_00" + str(i) + ".jpg"
         im = cv2.imread(image_no)
         h = hog.compute(im)
         inp_train.append(h)
@@ -24,8 +24,22 @@ def SVM_train(x_train, y_train):
     clf = svm.SVC(C = 5., gamma =0.001)
     clf.fit(x_train, y_train)
     return clf
-
+    
+def resizeImages(path, totalNo, size):
+    for i in xrange(1,totalNo):
+        if i<10:
+            image_no = path + "image_000" + str(i) + ".jpg"
+            image_no_mod = path + "imagemod_000" + str(i)+ ".jpg"
+        else:
+            image_no = path + "image_00" + str(i) + ".jpg"
+            image_no_mod = path + "imagemod_00" + str(i)+ ".jpg"
+        cmd = "convert "+image_no+" -resize " + size + "! " + image_no_mod
+        #~ print cmd
+        os.system(cmd)
+        
 if __name__ == "__main__":
+    #~ resizeImages("/home/tusharmakkar08/Desktop/ImageProcessing/Data/TotalPositive/",79, "200x128")
+    #~ resizeImages("/home/tusharmakkar08/Desktop/ImageProcessing/Data/TotalNegative/",5, "200x128")
     x_pos_train, y_pos_train = trainSample("/home/tusharmakkar08/Desktop/ImageProcessing/Data/TotalPositive/", 3)
     x_neg_train, y_neg_train = trainSample("/home/tusharmakkar08/Desktop/ImageProcessing/Data/TotalNegative/", 3)
     x_train = x_pos_train + x_neg_train
