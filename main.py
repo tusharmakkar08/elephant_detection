e7f02fa0c2633d25929aa8471e4e7d9e2bb805aa
Tushar Makkar <tusharmakkar08@gmail.com>
2015-04-13 10:41:31 +0530

    Svm training module added

--- main.py
+++ main.py
@@ -6,17 +6,28 @@ def train_positive():
     Training positive elephant sample
     '''
     hog = cv2.HOGDescriptor()
-    pos_train = []
-    for i in xrange(1,79):
+    inp_pos_train = []
+    out_pos_train = []
+    for i in xrange(1,3):
         if i<10:
-            image_no = "/home/tusharmakkar08/Desktop/ImageProcessing/Data/Total/image_000"+str(i)+".jpg"
+            image_no = "/home/tusharmakkar08/Desktop/ImageProcessing/Data/TotalPositive/image_000"+str(i)+".jpg"
         else:
-            image_no = "/home/tusharmakkar08/Desktop/ImageProcessing/Data/Total/image_00"+str(i)+".jpg"
+            image_no = "/home/tusharmakkar08/Desktop/ImageProcessing/Data/TotalPositive/image_00"+str(i)+".jpg"
         im = cv2.imread(image_no)
         h = hog.compute(im)
         inp_pos_train.append(h)
+        print i,len(h)
         out_pos_train.append(1)
     return inp_pos_train, out_pos_train
+    
+
+    
+def SVM_train(x_train, y_train):
+    clf = svm.SVC(C = 5., gamma =0.001)
+    clf.fit(x_train, y_train)
+    return clf
 
 if __name__ == "__main__":
-    print train_positive()
+    x_pos_train, y_pos_train = train_positive()
+    x_neg_train, y_neg_train = train_negative()
+    main_classifier = SVM_train(x_train, y_train)
