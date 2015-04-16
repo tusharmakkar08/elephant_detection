3e6836212f93bff9db9f8132a36de12d2a42cfcb
Tushar Makkar <tusharmakkar08@gmail.com>
2015-04-16 11:03:30 +0530

    Testing code added

--- main.py
+++ main.py
@@ -42,14 +42,17 @@ def testImage(hog, path, clf):
     clf : SVM Classifier
     OUTPUT 
     Prints whether elephant or not
+    Return 1 if elephant detected else 0
     '''
     im = cv2.imread(path)
     h = hog.compute(im).ravel()
     #~ print clf.predict(h)
     if clf.predict(h)[0] == 1 : 
         print "Elephant Detected"
+        return 1
     else: 
         print "No Elephant Is Detected"
+        return 0
 
 def SVM_train(x_train, y_train):
     '''
@@ -111,19 +114,57 @@ def loadClassifier(path):
         return classifier
     except :
         return False
-                
+
+def testAccuracy(path, N, hog, main_classifier):
+    '''
+    Testing images and the accuracy of the classifier
+    INPUT
+    path : Where test files are there
+    N : number of files to be tested
+    hog : hog file descriptor
+    main_classifier : Classifier for elephant detection
+    OUTPUT
+    accuracy : Percentage accuracy
+    '''
+    accuracyParameter = 0
+    fp = 0
+    fn = 0 
+    for i in xrange(1, N+1):
+        if i<10:
+            image_no = path + "imagemod_000" + str(i) + ".jpg"
+        else:
+            image_no = path + "imagemod_00" + str(i) + ".jpg"
+        if (i <= N/2):
+            if (testImage(hog, image_no, main_classifier) == 0):
+                accuracyParameter += 1
+            else:
+                fn += 1
+                print image_no
+        else:
+            if (testImage(hog, image_no, main_classifier) == 1):
+                accuracyParameter += 1
+            else:
+                fp += 1
+                print image_no
+    print accuracyParameter
+    return accuracyParameter*100.0 / (N*1.0), fn*100.0 / (N*1.0), fp*100.0 / (N*1.0) 
+    
 if __name__ == "__main__":
     '''
     Main function where everything is called
     '''
-    #~ resizeImages("/home/tusharmakkar08/Desktop/ImageProcessing/Data/TotalPositive/", 78, "200x128")
-    #~ resizeImages("/home/tusharmakkar08/Desktop/ImageProcessing/Data/TotalNegative/", 78, "200x128")
+    #~ resizeImages("/home/tusharmakkar08/Desktop/ImageProcessing/Data/TotalPositive/", 79, "200x128")
+    #~ resizeImages("/home/tusharmakkar08/Desktop/ImageProcessing/Data/TotalNegative/", 99, "200x128")
+    #~ resizeImages("/home/tusharmakkar08/Desktop/ImageProcessing/Data/Test/", 20, "200x128")
     hog = cv2.HOGDescriptor()
-    #~ x_pos_train, y_pos_train = trainSample(hog, "/home/tusharmakkar08/Desktop/ImageProcessing/Data/TotalPositive/", 78, 1)
-    #~ x_neg_train, y_neg_train = trainSample(hog, "/home/tusharmakkar08/Desktop/ImageProcessing/Data/TotalNegative/", 78, 0)
+    #~ x_pos_train, y_pos_train = trainSample(hog, "/home/tusharmakkar08/Desktop/ImageProcessing/Data/TotalPositive/", 79, 1)
+    #~ x_neg_train, y_neg_train = trainSample(hog, "/home/tusharmakkar08/Desktop/ImageProcessing/Data/TotalNegative/", 99, 0)
     #~ x_train = x_pos_train + x_neg_train
     #~ y_train = y_pos_train + y_neg_train
     #~ main_classifier = SVM_train(x_train, y_train)
     #~ saveClassifier(main_classifier)
     main_classifier = loadClassifier('classifier.pkl')
-    testImage(hog, "/home/tusharmakkar08/Desktop/ImageProcessing/Data/Test/test.jpg", main_classifier)
+    accuracy, fn, fp = testAccuracy("/home/tusharmakkar08/Desktop/ImageProcessing/Data/Test/", 20, hog, main_classifier)
+    print "Overall Accuracy Percentage of Classifier is ", accuracy, "%"
+    print "False Negative Percentage of Classifier is ", fn, "%"
+    print "False Positive Percentage of Classifier is ", fp, "%"
